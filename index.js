03d64981135c64fa2f591fb40d3119144b6880ee
zackdaspy <zahedajafari@gmail.com>
2023-08-28 14:11:48 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const element = (
+  // Write your code here.
+  <div className="Container">
+    <h1>Congratulations</h1>
+    <div className="insideCard">
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png" />
+      <h2>Kiran V</h2>
+      <p>Vishnu Institute of Computer Education and Technology. Bhimavaram</p>
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png" />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
